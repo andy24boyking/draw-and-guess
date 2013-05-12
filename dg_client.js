0925c498949fd169596360f15f7f64a33c34ddb6
andy24boyking <andy24boyking@126.com>
2013-05-12 15:08:34 +0800

    rm some test files

--- dg_client.js
+++ dg_client.js
@@ -1,10 +1,12 @@
-var IP = "10.12.52.223";
+var IP = "192.168.1.126";
 var socket = io.connect("http://"+IP+":8888");
 var draw;
 var name = "";
-//var id = 0;
-var content='';
-var status = 0;
+var sessionid = 0;
+var content="";
+var roomlist = [];
+var currentRoomId = 0;
+var status = 0; //0：未登录，1：房间列表界面，2：游戏界面
 
 function onLoad() {
 	draw = new Drawbox(document.getElementById('myCanvas'));
@@ -12,37 +14,27 @@ function onLoad() {
 
 	socket.on("connect", function() {
 		console.log("connected..");
-		socket.send("hi");
+		sessionid = socket.socket.sessionid;
 	});
-
+	socket.on("initroomlist", function(list) {
+		roomlist = list;
+	});
 	//更新新加用户
 	socket.on('newuser', function(newname) {
-		if(1 == status) {
-			//在列表中更新
-			/*var sel = document.getElementById("selectUser");
-			sel.options.add(new Option(newname.newname, newname.id));*/
-			//在对话框中提示
-			content += "<span id='name'>"+newname.newname+"</span> 上线！<br/>";
+		if(status != 0) {
+			/*content += "<span id='name'>"+newname.newname+"</span> 上线！<br/>";
 			document.getElementById("content").innerHTML = content;
-
-			setScroll();
+			setScroll();*/
 		}
 	});
 
 	//有人退出的处理
 	socket.on('exit2', function(exit) {
 		//通知有人退出
-		content += "<span id='name'>"+exit.name+"</span> 离开聊天室！<br/>";
+		/*content += "<span id='name'>"+exit.name+"</span> 离开聊天室！<br/>";
 		document.getElementById("content").innerHTML = content;
 
-		setScroll();
-		//更新用户列表
-		/*var selt = document.getElementById("selectUser");
-		for(var j = 0; j < selt.options.length; j++) {
-			if (exit.id == selt.options[j].value) {
-				selt.options.remove(j);
-			}
-		}*/
+		setScroll();*/
 	});
 
 	//断开连接
@@ -51,16 +43,100 @@ function onLoad() {
 		content = "错误: 已断开与服务器的连接";
 		document.getElementById("content").innerHTML = content;
 	});
+
+	//房间列表更新处理
+	socket.on("roomchange", function(list) {
+		roomlist = list;
+		if(1 == status) {
+			updateRoomlist();
+		}else if(2 == status) {
+			updateCurrentRoomPlayersList();
+		}
+	});
 }
 
 function login() {
 	name = document.getElementById("username").value;
 	//记录登录名
-	socket.emit('username', {username: name});
+	socket.emit("username", {username: name});
+	console.log(sessionid);
 	document.getElementById("uname").innerHTML = "欢迎 "+name+" 加入游戏!";
 
 	//登陆界面跳转
 	document.getElementById("login").style.display = "none";
-	document.getElementById("dg").style.display = "";
+	document.getElementById("room").style.display = "";
+	updateRoomlist();
 	status = 1;
+}
+
+//滚动条置于聊天窗口底部
+function setScroll() {
+	document.getElementById("content").scrollTop = document.getElementById("content").scrollHeight;
+}
+
+//创建房间
+function createRoom() {
+	document.getElementById("createroom").style.display = "";
+}
+function ok() {
+	var playNum = document.getElementById("playNum").value;
+	socket.emit("createRoom", playNum);
+	status = 2;
+	document.getElementById("room").style.display = "none";
+	document.getElementById("dg").style.display = "";
+}
+
+//更新房间列表
+function updateRoomlist() {
+	var text = "";
+	for(var i in roomlist) {
+		var count = 0;
+		for(var k in roomlist[i])
+			if(roomlist[i][k] != null && roomlist[i][k] != "")
+				count++;
+		text += "<a onclick='enterRoom("+i+","+count+")'> Room "+i+" , "+count+"/"+roomlist[i].length+"</a><br/>";
+	}
+	document.getElementById("list").innerHTML = text;
+}
+
+//进入房间
+function enterRoom(i, count) {
+	if(count == roomlist[i].length)
+		alert("该房间已满！");	
+	else {
+		status = 2;
+		socket.emit("enterroom", i);
+		for(var j = 0; j < roomlist[i].length; j++) {
+			if(roomlist[i][j] == null || roomlist[i][j] == "") {
+				roomlist[i][j] == sessionid;
+				break;
+			}
+		}
+		currentRoomId = i;
+
+		document.getElementById("room").style.display = "none";
+		document.getElementById("dg").style.display = "";
+		updateCurrentRoomPlayersList();
+	}
+}
+
+//退出房间
+function exitRoom() {
+	socket.emit("exitroom", currentRoomId)
+	status = 1;
+	document.getElementById("room").style.display = "";
+	document.getElementById("dg").style.display = "none";
+}
+
+//更新当前房间内用户列表
+function updateCurrentRoomPlayersList() {
+	var text = "";
+
+	for(var k in roomlist[currentRoomId])
+		if(roomlist[currentRoomId][k] != null && roomlist[currentRoomId][k] != "")
+			text += roomlist[currentRoomId][k]+" ";
+		else
+			text += "null ";
+
+	document.getElementById("currentRoomlist").innerHTML = text;
 }
--- dg_server.js
+++ dg_server.js
@@ -2,41 +2,58 @@ var baseServer = require("./baseServer");
 //启动http服务器
 server = baseServer.start();
 
-var userlist=[];
-var idCount = 0;
+var userlist = [];
+var roomlist = [];
+
 //创建websocket连接
-var socket = require('socket.io').listen(server, {log: false});
+var io = require('socket.io').listen(server, {log: false});
 
-socket.on("connection", function(es) {
-	es.on("message", function(data) {
-		console.log("recv "+data);
-	});
+io.sockets.on("connection", function(es) {
+	console.log("connected..");
+	es.emit("initroomlist", roomlist);
 	//记录登录名
-	es.on('username', function(username) {
-		console.log('username: '+username.username);
-
+	es.on("username", function(username) {
 		//插入至用户名表
-		var id = addUser(username.username);
-		//提供当前用户名列表
+		/*var id = */addUser(username.username, es.id);
+
+		/*//提供当前用户名列表
 		es.emit('namelist', {userlist: userlist, id: id});
 		//向已有用户广播新用户登录
-		es.broadcast.emit('newuser', {newname: username.username, id: id});
+		es.broadcast.emit('newuser', {newname: username.username, id: id});*/
 	});
 
 	//断开连接
 	es.on('disconnect', function(){
-		console.log(' disconnected...');
-	});
-
-	es.on('exit', function(data) {
-		console.log('exit: '+data.id);
+		console.log(' disconnected...'+es.id);
+		delUser(es.id);//从用户列表中将该用户删除
 		//通知有人退出
-		es.broadcast.emit('exit2', { id: data.id, name: userlist[data.id][1]});
-		//处理用户列表
-		delUser(data.id);//从用户列表中将该用户删除
-		
+		//es.broadcast.emit('exit2', { id: data.id, name: userlist[data.id][1]});
 	});
 
+
+	//创建房间
+	es.on("createRoom", function(num) {
+		var room = new Array(parseInt(num));
+		room[0] = es.id;
+		addRoom(room);
+
+		es.emit("roomchange", roomlist);
+		es.broadcast.emit("roomchange", roomlist);
+	});
+	//有人加入房间
+	es.on("enterroom", function(roomid) {
+		enterRoom(roomid, es.id);
+
+		es.broadcast.emit("roomchange", roomlist);
+	});
+	//退出房间
+	es.on("exitroom", function(roomid) {
+		var exitr = exitroom(roomid, es.id);
+		
+		es.emit("roomchange", roomlist);
+		es.broadcast.emit("roomchange", roomlist);
+	});
+	
 	es.on("his", function(data) {
 		es.broadcast.emit("toDraw", {his: data.his});
 	});
@@ -46,25 +63,65 @@ socket.on("connection", function(es) {
 });
 
 //添加用户至用户列表
-function addUser(name) {
-	for(var k in userlist) {
-		if('' == userlist[k][1]) {
-			userlist[k][1] = name;
+function addUser(name, id) {
+	for(var k = 0; k < userlist.length; k++) {
+		if(userlist[k] == undefined) {
+			userlist[k] = [id, name];
 			return k;
 		}
 	}
-	var user = [idCount, name];
+	var user = [id, name];
 	userlist.push(user);
-	idCount++;
-	return (idCount-1);
+
+	return userlist.length - 1;
 }
 
 //从用户列表中删除用户
 function delUser(id) {
 	for(var k in userlist) {
 		if(id == userlist[k][0]) {
-			userlist[k][1] = '';
+			delete userlist[k];
 			break;
 		}
 	}
+}
+
+//增加一个房间
+function addRoom(room) {
+	for(var k = 0; k < roomlist.length; k++)
+		if(roomlist[k] == undefined)
+			roomlist[k] = room;
+
+	roomlist.push(room);
+}
+//加入房间
+function enterRoom(roomid, id) {
+	for(var i = 0; i < roomlist[roomid].length; i++) {
+		if(roomlist[roomid][i] == null || roomlist[roomid][i] == "") {
+			roomlist[roomid][i] == id;
+			return;
+		}
+	}
+}
+//从房间中退出
+function exitroom(roomid, id) {
+	var count = 0;
+
+	for(var i = 0; i < roomlist[roomid].length; i++) {
+		if(roomlist[roomid][i] == id) {
+			roomlist[roomid][i] = "";
+			break;
+		}
+	}
+
+	for(var i = 0; i < roomlist[roomid].length; i++) {
+		if(roomlist[roomid][i] != null && roomlist[roomid][i] != "")
+			count++;
+	}
+
+	if(count == 0) {
+		delete roomlist[roomid];
+		return 0;
+	}else
+		return 1;
 }
